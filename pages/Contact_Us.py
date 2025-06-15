6bc7c24f84f5078af34c4c84630ec4b8b1c3146f
jordykenfack <158819629+jordykenfack@users.noreply.github.com>
2025-06-15 15:24:11 +0300

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Us ")
+
+with st.form(key='form'):
+    user_email = st.text_input('your email address')
+    raw_message =  st.text_area('enter your message')
+    message = f"""\
+    Subject: New mail 
+    
+    From: {user_email}
+    {raw_message}
+    
+"""
+    button = st.form_submit_button('submit')
+    if button:
+        send_email(message)
+        st.info('Email sent successfully')
---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    user_name = "jordykenfack@gmail.com"
+    password = "yatj zxoj jjqw mxqu"
+    message = """\
+subject:Greetings!
+
+How are you ?"""
+    receiver_email = "jordykenfack@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver_email, message)
+
+
+send_email()
